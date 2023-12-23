8a5c67a28d3f2fe9cd818a97cd6f82f46f853677
d-rens <d.renschler@icloud.com>
2023-12-23 11:15:10 +0100

    Added possibility to read all CSVs and plot it with the csv name

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 import csv
 from collections import Counter
 from matplotlib import pyplot as plt
+import os
 
-ipalist = []
 de_konst_und_vok = ['i', 'y', 'u', 'ɪ', 'e', 'ʏ', 'ø', 'ʊ', 'o', 'ɛ', 'œ', 'ɐ', 'ɔ', 'a',
                     'm', 'n', 'ŋ',
-                    'p', 't', 'k','ʔ',
-                    'b', 'd', 'ɡ', 
+                    'p', 't', 'k', 'ʔ',
+                    'b', 'd', 'ɡ',
                     'f', 's', 'ʃ', 'ç', 'x', 'h',
                     'v', 'z', 'ʒ', 'j'
                     'l', 'r']
-print("reading csv")
 
-with open("datasets/dewikt.csv", mode='r', encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    for w in reader:
-        for l in list(w[1]):
-            if l in de_konst_und_vok:
-                ipalist += l
-print("csv read")
+csv_folder = "datasets/"
+output_folder = "output/"
 
-with open("output/counts.txt", mode='w',  encoding="utf-8") as output_file:
-    for sym, num in Counter(ipalist).items():
-        output_file.write(sym + " " + str(num) + "\n")
+for csv_file in os.listdir(csv_folder):
+    if csv_file.endswith(".csv"):
+        data = os.path.join(csv_folder, csv_file)
+        ipalist = []
 
-print("csv counted and saved in output/")
+        print(f"Reading CSV: {data}")
 
-print("start plotting")
-bars = []
+        with open(data, mode='r', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile)
+            for w in reader:
+                for l in list(w[1]):
+                    if l in de_konst_und_vok:
+                        ipalist += l
 
-with open("output/counts.txt", "r") as file:
-    for line in file:
-        bar_name, bar_height = line.split()
-        bars.append((bar_name, int(bar_height)))
-print("read counts.txt")
+        output_filename = f"{csv_file.split('.')[0]}_counts.txt"
 
-bars = sorted(bars, key=lambda x: x[1], reverse=True)
-print("sorted bars")
+        with open(os.path.join(output_folder, output_filename), mode='w', encoding="utf-8") as output_file:
+            for sym, num in Counter(ipalist).items():
+                output_file.write(sym + " " + str(num) + "\n")
 
-bar_names, bar_heights = zip(*bars)
+        print(f"CSV counted and saved in {output_filename}")
 
-plt.bar(bar_names, bar_heights)
+        print("Start plotting")
+        bars = []
 
-print("saved plot in output/")
-plt.savefig("output/plot.png")
+        with open(os.path.join(output_folder, output_filename), "r") as file:
+            for line in file:
+                bar_name, bar_height = line.split()
+                bars.append((bar_name, int(bar_height)))
+
+        # bars = sorted(bars, key=lambda x: x[1], reverse=True)
+
+        bar_names, bar_heights = zip(*bars)
+
+        plt.title(csv_file)
+        plt.bar(bar_names, bar_heights)
+
+        plt.savefig(os.path.join(output_folder, f"{csv_file.split('.')[0]}_plot.png"))
+        plt.clf()  # Clear the current figure to start a new one for the next iteration
+
+        print(f"Saved plot in {output_folder}/{csv_file.split('.')[0]}_plot.png")
